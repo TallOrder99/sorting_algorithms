6027984d54d056d10981a1c3df0aa2aebd191bf8
TallOrder99 <99543504+TallOrder99@users.noreply.github.com>
2022-07-23 22:36:02 +0300

    Update 107-quick_sort_hoare.c

--- 107-quick_sort_hoare.c
+++ 107-quick_sort_hoare.c
@@ -1,80 +1,76 @@
 #include "sort.h"
-int hoare_partition(int *, int, int);
-void quick_(int *, int, int);
-void swap(int *, int, int);
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
- *quick_sort_hoare - sorts an array of integers in ascending
- *order using the Quick sort algorithm
- *@array: list
- *@size:array size
+ * partition - finds the partition for the quicksort using the Hoare scheme
+ * @array: array to sort
+ * @lo: lowest index of the partition to sort
+ * @hi: highest index of the partition to sort
+ * @size: size of the array
+ *
+ * Return: index of the partition
+ */
+size_t partition(int *array, ssize_t lo, ssize_t hi, size_t size)
+{
+	int swap, pivot;
+
+	pivot = array[hi];
+	while (lo <= hi)
+	{
+		while (array[lo] < pivot)
+			lo++;
+		while (array[hi] > pivot)
+			hi--;
+		if (lo <= hi)
+		{
+			if (lo != hi)
+			{
+				swap = array[lo];
+				array[lo] = array[hi];
+				array[hi] = swap;
+				print_array(array, size);
+			}
+			lo++;
+			hi--;
+		}
+	}
+	return (hi);
+}
+
+/**
+ * quicksort - sorts a partition of an array of integers
+ * @array: array to sort
+ * @lo: lowest index of the partition to sort
+ * @hi: highest index of the partition to sort
+ * @size: size of the array
+ *
+ * Return: void
+ */
+void quicksort(int *array, ssize_t lo, ssize_t hi, size_t size)
+{
+	ssize_t pivot;
+
+	if (lo < hi)
+	{
+		pivot = partition(array, lo, hi, size);
+		quicksort(array, lo, pivot, size);
+		quicksort(array, pivot + 1, hi, size);
+
+	}
+}
+
+/**
+ * quick_sort_hoare - sorts an array of integers in ascending order using the
+ * Quick sort algorithm
+ * @array: The array to sort
+ * @size: The size of the array
+ *
+ * Return: void
  */
 void quick_sort_hoare(int *array, size_t size)
 {
-	if (array == NULL || size < 2 || size == 0)
+	if (array == NULL || size < 2)
 		return;
-	quick_(array, 0, size - 1);
-}
-/**
- *hoare_partition - partition to right high and left low
- *@list: array list
- *@left: low index
- *@right: right index
- *Return: index
- */
-int hoare_partition(int *list, int left, int right)
-{
-	int pivot = list[right];
-	int i = left - 1, j = right + 1;
-	static int size, c;
-
-	if (c == 0)
-		size = right + 1, c++;
-
-	while (list)
-	{
-		do {
-			i++;
-		} while (list[i] < pivot);
-		do {
-			j--;
-		} while (list[j] > pivot);
-
-		if (i >= j)
-			return (j);
-		swap(list, i, j);
-		print_array(list, size);
-	}
-	swap(list, i, j);
-	print_array(list, size);
-	return (j);
-}
-/**
- *quick_ - pass element index
- *@list: array list
- *@left: left index
- *@right: right index
- */
-void quick_(int *list, int left, int right)
-{
-	int pi;
-
-	if (left < right)
-	{
-		pi = hoare_partition(list, left, right);
-		quick_(list, left, pi - 1);
-		quick_(list, pi, right);
-	}
-}
-/**
- *swap - exchange value
- *@list: array
- *@x: left index
- *@y: right index
- */
-void swap(int *list, int x, int y)
-{
-	int temp = list[x];
-
-	list[x] = list[y];
-	list[y] = temp;
+	quicksort(array, 0, size - 1, size);
 }
